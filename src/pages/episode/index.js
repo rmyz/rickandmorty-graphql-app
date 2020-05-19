857732c6853a91f33f9de28267e9f6c5b6bcbc63
Sergi Romeu <romeuhd@gmail.com>
2020-05-19 02:44:13 +0200

    rename routes

---
+++ src/pages/episode/index.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+import { client } from '../../services/ApolloClient';
+import { GET_EPISODES } from '../../queries';
+import Episodes from '../../components/Episodes';
+
+export async function getServerSideProps() {
+  const {
+    data: { episodes },
+  } = await client.query({ query: GET_EPISODES, variables: { page: 1 } });
+
+  return { props: { episodes } };
+}
+
+const EpisodesRoute = ({ episodes }) => (
+  <>
+    <Head>
+      <title>Episodes - Rick and Morty</title>
+    </Head>
+    <Episodes episodes={episodes} />
+  </>
+);
+
+EpisodesRoute.propTypes = {
+  episodes: PropTypes.object,
+};
+
+export default EpisodesRoute;
