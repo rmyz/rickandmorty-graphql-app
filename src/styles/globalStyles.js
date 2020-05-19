56e1e3333c29d062d6a981fa6e7738877b085f41
Sergi Romeu <romeuhd@gmail.com>
2020-05-19 02:42:52 +0200

    add custom scrollbar

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -9,6 +9,27 @@ const GlobalStyles = createGlobalStyle`
     background: #121212;
     color: #ddd;
   }
+
+  ::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #212020;
+  border-radius: 5px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 5px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #ddd;
+}
 `;
 
 export default GlobalStyles;
